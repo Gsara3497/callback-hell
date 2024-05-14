711a44c23bfb4bb3fe38207ccd2857ee272fff75
Gsara3497 <vijayasekar001s@gmail.com>
2024-05-14 21:23:42 +0530

    This folder is used to create js coding of the task

---
+++ js/script.js
@@ -0,0 +1,71 @@
+// Countdown from 10 to 1
+
+ const countdown=document.createElement("countdown");
+setTimeout(() => {
+    document.getElementById('countdown').textContent=10;
+    setTimeout(() => {
+      document.getElementById('countdown').textContent=9;
+      setTimeout(() => {
+        document.getElementById('countdown').textContent=8;
+        setTimeout(() => {
+          document.getElementById('countdown').textContent=7;
+          setTimeout(() => {
+            document.getElementById('countdown').textContent=6;
+            setTimeout(() => {
+              document.getElementById('countdown').textContent=5;
+              setTimeout(() => {
+                document.getElementById('countdown').textContent=4;
+                setTimeout(() => {
+                  document.getElementById('countdown').textContent=3;
+                  setTimeout(() => {
+                    document.getElementById('countdown').textContent=2;
+                    setTimeout(() => {
+                      document.getElementById('countdown').textContent=1;
+                      setTimeout(() => {
+                        document.getElementById('countdown').textContent= "Happy Independence Day";
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+  // Countdown from 10 to 1
+setTimeout(() => {
+    document.getElementById('countdown').textContent=10;
+    setTimeout(() => {
+      document.getElementById('countdown').textContent=9;
+      setTimeout(() => {
+        document.getElementById('countdown').textContent=8;
+        setTimeout(() => {
+          document.getElementById('countdown').textContent=7;
+          setTimeout(() => {
+            document.getElementById('countdown').textContent=6;
+            setTimeout(() => {
+              document.getElementById('countdown').textContent=5;
+              setTimeout(() => {
+                document.getElementById('countdown').textContent=4;
+                setTimeout(() => {
+                  document.getElementById('countdown').textContent=3;
+                  setTimeout(() => {
+                    document.getElementById('countdown').textContent=2;
+                    setTimeout(() => {
+                      document.getElementById('countdown').textContent=1;
+                      setTimeout(() => {
+                        document.getElementById('countdown').textContent= "Happy Independence Day";
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+  
